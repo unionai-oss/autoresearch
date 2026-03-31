b6344fe8e98eac79cdcc77d9e762a2b5ccb4a43c
parnianz <parnianzargham@gmail.com>
2026-03-31 20:08:25 +0000

    Experiment 11: set FINAL_LR_FRAC to 0.1

--- train.py
+++ train.py
@@ -508,7 +508,7 @@ WEIGHT_DECAY = 0.1      # cautious weight decay for Muon - reduced
 ADAM_BETAS = (0.9, 0.95) # Adam beta1, beta2 - higher momentum
 WARMUP_RATIO = 0.05     # fraction of time budget for LR warmup - added some warmup
 WARMDOWN_RATIO = 0.3    # fraction of time budget for LR warmdown - reduced
-FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
+FINAL_LR_FRAC = 0.1     # final LR as fraction of initial - keep some LR at end
 
 # Model size
 DEPTH = 4               # number of transformer layers (reduced for Tesla T4)
