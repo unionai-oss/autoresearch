c0548d072b83688e5bdbc4158b390d0b49d1d779
parnianz <parnianzargham@gmail.com>
2026-04-08 16:46:35 +0000

    Experiment 7: set FINAL_LR_FRAC to 0.1 to maintain learning near end of training

--- train.py
+++ train.py
@@ -462,7 +462,7 @@ WEIGHT_DECAY = 0.2      # cautious weight decay for Muon
 ADAM_BETAS = (0.8, 0.95) # Adam beta1, beta2
 WARMUP_RATIO = 0.0      # fraction of time budget for LR warmup
 WARMDOWN_RATIO = 0.2    # fraction of time budget for LR warmdown (more training at higher LR)
-FINAL_LR_FRAC = 0.0     # final LR as fraction of initial
+FINAL_LR_FRAC = 0.1     # final LR as fraction of initial (don't go to zero)
 
 # Model size
 DEPTH = 4               # number of transformer layers (reduced for T4)
